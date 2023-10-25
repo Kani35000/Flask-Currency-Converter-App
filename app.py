8ca5812e39bcb9704d4f7653098f88b44f6b37c2
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-25 01:56:56 -0400

    adding timeline link

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app= Flask(__name__)
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True 
